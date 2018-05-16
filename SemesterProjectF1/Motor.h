b518071709e7357353cb8859997ababb6b1be89f
Hogfeldt <hogfeldt38@gmail.com>
2018-05-16 11:29:47 +0200

    Header file for Motor class is added. Motor class is defined. Enum called direction is defined in header, for the purpose of controlling motor direction

---
+++ SemesterProjectF1/Motor.h
@@ -0,0 +1,16 @@
+/*
+ * Motor.h
+ *
+ * Created: 16/05/2018 11.05.52
+ *  Author: hogfe
+ */ 
+
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+
+
+
+
+#endif /* MOTOR_H_ */
---
+++ SemesterProjectF1/direction.h
@@ -0,0 +1,16 @@
+/*
+ * direction.h
+ *
+ * Created: 16/05/2018 11.13.04
+ *  Author: hogfe
+ */ 
+
+
+#ifndef DIRECTION_H_
+#define DIRECTION_H_
+
+
+
+
+
+#endif /* DIRECTION_H_ */

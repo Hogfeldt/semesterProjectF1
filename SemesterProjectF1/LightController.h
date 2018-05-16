31bfaf31f6e90e21d2ff057c47cddb293a0c2358
Hogfeldt <hogfeldt38@gmail.com>
2018-05-16 11:05:11 +0200

    Header file for LightController class is added. LightController class is defined

---
+++ SemesterProjectF1/LightController.h
@@ -0,0 +1,47 @@
+/*
+ * File: LightController.h
+ * Created: 16/05/2018 10.28.11
+ * Author: Per Hogfeldt
+ */ 
+
+#ifndef LIGHTCONTROLLER_H_
+#define LIGHTCONTROLLER_H_
+/**
+ * LightController is responsible for controlling 
+ * the LED light on the car
+ */
+class LightController {
+public:
+	/**
+	 * Turn on the front and back light
+	 * Precondition: none
+     * @param: none
+     * @return: void
+     */	
+	void turnOnDrivingLight();
+	
+	/**
+	 * Turn off the front and back light
+	 * Precondition: none
+     * @param: none
+     * @return: void
+     */	
+	void turnOffDrivingLight();
+	
+	/**
+	 * The backlight should light brighter
+	 * Precondition: Driving light is on
+     * @param: none
+     * @return: void
+     */	
+	void turnOnBreakLight();
+	
+	/**
+	 * The back light should light normally
+	 * Precondition: Driving light is on
+     * @param: none
+     * @return: void
+     */	
+	void turnOffBreakLight();
+};
+#endif /* LIGHTCONTROLLER_H_ */

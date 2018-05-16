8c7ad9a7cbbd9486eec3b5eebf481499adcd88f9
Hogfeldt <hogfeldt38@gmail.com>
2018-05-16 10:24:27 +0200

    Syntax error is fixed in AudioController.h

--- SemesterProjectF1/AudioController.h
+++ SemesterProjectF1/AudioController.h
@@ -1,16 +1,38 @@
 /*
- * AudioController.h
- *
+ * File: AudioController.h
  * Created: 16/05/2018 09.49.56
- *  Author: hogfe
+ * Author: Per Hoegfeldt
  */ 
-
 
 #ifndef AUDIOCONTROLLER_H_
 #define AUDIOCONTROLLER_H_
-
-
-
-
-
+/**
+ * AudioController is responsible for controlling the SOMO-II mp3
+ */
+class AudioController{
+public:
+	/**
+	 * Play the start sound
+	 * Precondition: none
+     * @param: none
+     * @return: void
+     */	
+	void playStartSound();
+	
+	/**
+	 * Play the stop sound
+	 * Precondition: none
+     * @param: none
+     * @return: void
+     */	
+	void playStopSound();
+	
+	/**
+	 * Play the stop reflect sound
+	 * Precondition: none
+     * @param: none
+     * @return: void
+     */	
+	void playReflectSound();
+};
 #endif /* AUDIOCONTROLLER_H_ */
